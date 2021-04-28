3183f913f9d675f69f27bf504f6ccd8a86a3cb61
Floris <f.a.j.dorrestijn@students.uu.nl>
2021-04-28 11:10:35 +0200

    Copyright notices + explanation

--- Crawler/RepositoryCrawler.h
+++ Crawler/RepositoryCrawler.h
@@ -1,3 +1,9 @@
+/*
+This program has been developed by students from the bachelor Computer Science at
+Utrecht University within the Software Project course.
+© Copyright Utrecht University (Department of Information and Computing Sciences)
+*/
+
 #pragma once
 #include <vector>
 #include "RunCrawler.h"
@@ -7,6 +13,11 @@ class RepositoryCrawler
 private:
 	GithubInterface *githubInterface;
 public:
+	/// <summary>
+	/// Returns a list of URLs to be crawled.
+	/// </summary>
+	/// <param name="crawlableSource">The source which needs to be crawled.</param>
+	/// <returns>A list of URLs.</returns>
 	std::vector<std::string> crawlRepositories(CrawlableSource crawlableSource);
 	RepositoryCrawler(GithubInterface* githubInterface) {
 		this->githubInterface = githubInterface;
