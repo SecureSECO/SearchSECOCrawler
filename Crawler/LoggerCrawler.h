9929b10ae2f6c2050a471b9be8d96310966a6ca7
Jochem Bloem <j.bloem1@uu.nl>
2021-05-26 14:13:33 +0200

    Summaries in LoggerCrawler

--- Crawler/LoggerCrawler.h
+++ Crawler/LoggerCrawler.h
@@ -11,11 +11,30 @@ Utrecht University within the Software Project course.
 class LoggerCrawler
 {
 private:
+	/// <summary>
+	/// Formats a string and an error code to a correctly formatted error message.
+	/// </summary>
 	static std::string getMessage(const char *message, int code);
 
 public:
+	/// <summary>
+	/// Logs a message at Debug-level verbosity.
+	/// </summary>
 	static void logDebug(std::string message, const char* file, unsigned int line);
+
+	/// <summary>
+	/// Logs a message at Info-level verbosity.
+	/// </summary>
 	static void logInfo(std::string message, const char* file, unsigned int line);
+
+	/// <summary>
+	/// Logs a Warning.
+	/// </summary>
 	static void logWarn(std::string message, const char* file, unsigned int line);
+
+	/// <summary>
+	/// Logs a Fatal (Error) message. This does not terminate execution, that responsibility lies 
+	/// with the Controller component.
+	/// </summary>
 	static void logFatal(std::string message, const char* file, unsigned int line, int code);
 };
