a44395c4fe611a3f933ca14216ad5aaf5bd61791
Floris Dorrestijn <f.a.j.dorrestijn@students.uu.nl>
2021-06-24 16:14:08 +0200

    Refactoring and adding comments

--- Crawler/GithubInterface.cpp
+++ Crawler/GithubInterface.cpp
@@ -19,14 +19,24 @@ JSON* GithubInterface::getRequest(std::string query, GithubErrorThrowHandler *ha
 	curl::curl_ios<std::stringstream> writer(ss);
 	curl::curl_easy easy(writer);
 	// Set up data
-	easy.add<CURLOPT_URL>(query.data());
-	easy.add<CURLOPT_FOLLOWLOCATION>(1L);
-	easy.add<CURLOPT_USERAGENT>(userAgent.data());
-	easy.add<CURLOPT_USERPWD>(userPWD.data());
+	addEasyInformation(query, easy);
 	
-	std::future<void> future = std::async(std::launch::async, [&easy]()
+	executeCurlQueryWithTimeout(easy);
+	LoggerCrawler::logDebug("CURL query done", __FILE__, __LINE__);
+	long responseCode = easy.get_info<CURLINFO_RESPONSE_CODE>().get();
+	
+	if (responseCode != 200)
 	{
-		// Send query
+		handleGithubError(easy, ss, handler, jsonHandler);
+	}
+
+	return JSON::parse(ss.str(), jsonHandler);
+}
+
+void GithubInterface::executeCurlQueryWithTimeout(curl::curl_easy &easy)
+{
+	// Put the curl query in an async task to allow for a timeout.
+	std::future<void> future = std::async(std::launch::async, [&easy]() {
 		try
 		{
 			LoggerCrawler::logDebug("Executing CURL query", __FILE__, __LINE__);
@@ -40,38 +50,53 @@ JSON* GithubInterface::getRequest(std::string query, GithubErrorThrowHandler *ha
 		}
 	});
 	std::future_status status;
+	// Wait for the curl query to start and return a result.
+	// If the status is 'deferred', then our async task has not yet started.
 	do
 	{
 		status = future.wait_for(std::chrono::seconds(waitTime));
 	} while (status == std::future_status::deferred);
+
+	// If the status is not ready (and not deferred either as we have already started the curl query), 
+	// we have a timeout.
 	if (status != std::future_status::ready)
 	{
 		LoggerCrawler::logWarn("CURL ran into a timeout", __FILE__, __LINE__);
 		throw 0;
 	}
-	LoggerCrawler::logDebug("CURL query done", __FILE__, __LINE__);
-	long responseCode = easy.get_info<CURLINFO_RESPONSE_CODE>().get();
-	std::string type = easy.get_info<CURLINFO_CONTENT_TYPE>().get();
-
-	if (responseCode != 200)
-	{
-		githubAPIResponse response;
-		if (type.find("application/json") != std::string::npos)
-		{
-			JSON *json = JSON::parse(ss.str(), jsonHandler);
-			std::string msg = json->get<std::string, std::string>("message");
-			response = GithubClientErrorConverter::convertResponse(responseCode, msg);
-			delete json;
-		}
-		else
-		{
-			response = GithubClientErrorConverter::convertResponse(responseCode, "");
-		}
-		handler->handle(response, __FILE__, __LINE__);
-	}
-
-	return JSON::parse(ss.str(), jsonHandler);
 }
+
+void GithubInterface::addEasyInformation(std::string query, curl::curl_easy &easy)
+{
+	easy.add<CURLOPT_URL>(query.data());
+	easy.add<CURLOPT_FOLLOWLOCATION>(1L);
+	easy.add<CURLOPT_USERAGENT>(userAgent.data());
+	easy.add<CURLOPT_USERPWD>(userPWD.data());
+}
+
+void GithubInterface::handleGithubError(curl::curl_easy &easy, std::stringstream &ss, GithubErrorThrowHandler *handler,
+					   JSONErrorHandler *jsonHandler)
+{
+	std::string type = easy.get_info<CURLINFO_CONTENT_TYPE>().get();
+	long responseCode = easy.get_info<CURLINFO_RESPONSE_CODE>().get();
+	githubAPIResponse response;
+
+	// If the type is JSON, we received a message next to our code in JSON format.
+	if (type.find("application/json") != std::string::npos)
+	{
+		JSON *json = JSON::parse(ss.str(), jsonHandler);
+		std::string msg = json->get<std::string, std::string>("message");
+
+		// Use the message we received besides the code to find out what exactly was the error we received.
+		response = GithubClientErrorConverter::convertResponse(responseCode, msg);
+		delete json;
+	}
+	else //Otherwise, we can just use the responsecode to see what has happened.
+	{
+		response = GithubClientErrorConverter::convertResponse(responseCode, "");
+	}
+	handler->handle(response, __FILE__, __LINE__);
+} 
 
 JSON *GithubInterface::getRequest(std::string query)
 {
--- Crawler/GithubInterface.h
+++ Crawler/GithubInterface.h
@@ -18,6 +18,29 @@ private:
 	std::string userPWD;
 	GithubErrorThrowHandler defaultHandler;
 
+	/// <summary>
+	/// Execute a curl query in a try catch, with a timeout to ensure that it always stops after a certain time.
+	/// </summary>
+	/// <param name="easy">The curl object.</param>
+	void executeCurlQueryWithTimeout(curl::curl_easy &easy);
+
+	/// <summary>
+	/// Add information to the curl query.
+	/// </summary>
+	/// <param name="query">The query that has to be added to the curl object.</param>
+	/// <param name="easy">The curl object.</param>
+	void addEasyInformation(std::string query, curl::curl_easy &easy);
+
+	/// <summary>
+	/// Handles a github error if one happened during the execution of the curl query.
+	/// </summary>
+	/// <param name="easy">The curl object.</param>
+	/// <param name="ss">The stringstream that is written to by the curl object.</param>
+	/// <param name="handler">The github handler.</param>
+	/// <param name="jsonHandler">The JSON handler.</param>
+	void handleGithubError(curl::curl_easy &easy, std::stringstream &ss, GithubErrorThrowHandler *handler,
+						   JSONErrorHandler *jsonHandler);
+
 public:
 	GithubInterface(std::string userAgent, std::string userPWD)
 	{
@@ -35,11 +58,17 @@ public:
 	/// <summary>
 	/// Requests data from Github and returns a pointer to a JSON variable.
 	/// </summary>
-	/// <param name="query">The query to be send to Github.</param>
+	/// <param name="query">The query to be sent to Github.</param>
 	/// <param name="handler">A pointer to a GithubErrorThrowHandler class.</param>
 	/// <param name="jsonHandler"> A pointer to a JSONErrorHandler class.</param>
 	/// <returns>A pointer to a JSON variable.</returns>
-	virtual JSON *getRequest(std::string query, GithubErrorThrowHandler *handler, JSONErrorHandler* jsonHandler);
+	virtual JSON *getRequest(std::string query, GithubErrorThrowHandler *handler, JSONErrorHandler *jsonHandler);
+
+	/// <summary>
+	/// Requests data from Github and returns a pointer to a JSON variable.
+	/// </summary>
+	/// <param name="query">The query to be sent to Github.</param>
+	/// <param name="handler">A pointer to a GithubErrorThrowHandler class.</param>
+	/// <returns>A pointer to a JSON variable.</returns>
 	virtual JSON* getRequest(std::string query, GithubErrorThrowHandler* handler);
-	//virtual JSON* getRequest(std::string query, JSONErrorHandler* jsonHandler);
 };
