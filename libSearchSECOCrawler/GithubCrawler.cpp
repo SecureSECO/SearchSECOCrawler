0b298897cf9e9a6f7c876cd665a906126237c292
Marien <matser71@gmail.com>
2022-03-22 13:26:55 +0100

    Crawl most important repos

--- libSearchSECOCrawler/GithubCrawler.cpp
+++ libSearchSECOCrawler/GithubCrawler.cpp
@@ -18,11 +18,11 @@ CrawlData GithubCrawler::crawlRepositories(int start)
 {
 	auto strStart = std::to_string(start);
 	GithubErrorThrowHandler *handler = getCorrectGithubHandler();
-	LoggerCrawler::logDebug("Starting crawling at index " + strStart, __FILE__, __LINE__);
+	LoggerCrawler::logDebug("Starting crawling at page " + strStart, __FILE__, __LINE__);
 	int currentId;
 
 	// Create an unique_ptr from a GitHub request asking for a list of repositories, and use that to get the CrawlData.
-	std::unique_ptr<JSON> json(githubInterface->getRequest("https://api.github.com/repositories?since=" + strStart));
+	std::unique_ptr<JSON> json(githubInterface->getRequest("https://api.github.com/search/repositories?q=stars:>1&sort=stars&page=" + strStart));
 	CrawlData crawlData;
 	if (errno != 0)
 	{
@@ -40,14 +40,15 @@ CrawlData GithubCrawler::crawlRepositories(int start)
 CrawlData GithubCrawler::getCrawlData(std::unique_ptr<JSON> &json, GithubErrorThrowHandler *handler, int &currentId)
 {
 	CrawlData crawlData;
+	JSON result = json->branch("items");
 	// The maximum amount of URLs we can crawl.
-	int bound = std::min(json->length(), MAXRESULTS);
+	int bound = std::min(result.length(), MAXRESULTS);
 
 	for (int i = 0; i < bound; i++)
 	{
 		logProgress(i, bound);
 
-		JSON branch = json->branch(i);
+		JSON branch = result.branch(i);
 		currentId = branch.get<std::string, int>("id", true);
 
 		// Add URL if possible, if we get a 0 skip this URL, if we get a 1 we have a fatal error.
